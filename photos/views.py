954f50bfee12badfa09d159a692f5566aa93cd10
mishel254 <sonimichie@gmail.com>
2020-11-17 18:21:37 +0300

    pctures

--- photos/views.py
+++ photos/views.py
@@ -6,7 +6,12 @@ from .models import Pictures,Editor, category
 def index(request):
     return render(request, 'index.html')
 
+def photos(request):
+   photos= Pictures.objects.all()
+   context = {'photos':photos}
+   return render(request,'photos.html',context)
 
+   
 def search_results(request):
 
     if 'category' in request.GET and request.GET["category"]:
@@ -14,15 +19,20 @@ def search_results(request):
         searched_category = Pictures.search_by_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'photos.html',{"message":message,"photos": searched_category})
+        return render(request, 'search.html',{"message":message,"photos": searched_category})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'photos.html',{"message":message})
+        return render(request, 'search.html',{"message":message})
 
-def photos(request):
-   photos= Pictures.objects.all()
-   context = {'photos':photos}
-   return render(request,'photos.html',context)
+
+
+
+def photo(request,photo_id):
+    try:
+        photo = Pictures.objects.get(id = photo_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"photo.html", {"photo":photo})
 
 
