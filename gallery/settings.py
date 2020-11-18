225e7e2754dd5bbb07a7587b95c64d4d41e2e5d8
mishel254 <sonimichie@gmail.com>
2020-11-18 08:18:17 +0300

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -97,14 +97,7 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'photos',
-        'USER': 'moringa',
-        'PASSWORD':'pa33word'
-    }
-}
+
 
 
 
